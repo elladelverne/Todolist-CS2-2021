0a3b20f5c3ed52834aced0763c9e1a6107d2956f
elladelverne <44614381+elladelverne@users.noreply.github.com>
2020-10-05 14:07:17 -0400

    Add files via upload

--- client.js
+++ client.js
@@ -1,1 +1,21 @@
 console.log('script loaded')
+
+//add click event handler on button
+let $button = document.getElementById('addTodo')
+let $textbox = document.getElementById('userInput')
+let $list = document.getElementById('list')
+$button.onclick = addTodo
+
+//define the button click handler
+function addTodo(event) {
+    //console.log("button was clicked")
+    //$button.style.backgroundColor = 'teal'
+    
+    //get text from text box
+    let newTodoText = $textbox.value
+    //creat new list item
+    let $newTodoItem = document.createElement('li')
+    $newTodoItem.innerHTML= newTodoText
+    //list item in list
+    $list.append($newTodoItem)
+}
