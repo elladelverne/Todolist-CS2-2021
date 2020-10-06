8af0d9149d607bab2e880f77b4569847602f597e
elladelverne <44614381+elladelverne@users.noreply.github.com>
2020-10-06 14:08:11 -0400

    Challenge Completed: Add option Delete!

--- client.js
+++ client.js
@@ -5,9 +5,9 @@ let $button = document.getElementById('addTodo')
 let $textbox = document.getElementById('userInput')
 let $list = document.getElementById('list')
 let $head = document.getElementById('header')
+let $completed = document.getElementById('completed')
 $button.onclick = addTodo
 $head.onclick = changeColor
-$head.ondblclick = changeColorTwo
 //define the button click handler
 function addTodo(event) {
     //console.log("button was clicked")
@@ -15,15 +15,26 @@ function addTodo(event) {
     
     //get text from text box
     let newTodoText = $textbox.value
+
+    $textbox.value = ''
     //creat new list item
     let $newTodoItem = document.createElement('li')
-    $newTodoItem.innerHTML= newTodoText
+    $newTodoItem.innerHTML= `${newTodoText}<button onclick='taskDone(event)'>Done</button><button onclick='deleteTask(event)'>Delete</button>`
+    
     //list item in list
     $list.append($newTodoItem)
 }
 function changeColor(){
     $head.style.color = 'red'
 }
-function changeColorTwo(){
-    $head.style.color = 'black'
+
+function taskDone(event){
+    let $listItem = event.target.parentElement
+    console.log($listItem)
+    event.target.remove()
+    $listItem.style.textDecoration='line-through'
+    $completed.append($listItem)
+}
+function deleteTask(event){
+    event.target.parentElement.remove()
 }
